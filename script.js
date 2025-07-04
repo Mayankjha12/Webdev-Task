6d3bc842d75483b9195f0d3d5bb2cb3cdd9149c2
Mayankjha12 <mayank.jha.ug24@nsut.ac.in>
2025-07-05 00:59:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.getElementById('modeToggle').addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
-  });
-  
-  document.getElementById('contactForm').addEventListener('submit', (e) => {
+});
+
+document.getElementById('contactForm').addEventListener('submit', (e) => {
   e.preventDefault();
   alert('Message sent successfully!');
-  });
+});
